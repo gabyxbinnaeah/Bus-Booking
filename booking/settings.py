3ff86e69edf6e4bd22b01162ec019fe305efe8f4
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-08-03 11:06:40 +0300

    creates userapp,adminapp and driverapp basic structure

--- booking/settings.py
+++ booking/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bus_booking',
+    'userapp',
+    'driverapp',
+    'adminapp',
 ]
 
 MIDDLEWARE = [
