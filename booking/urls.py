0bcb94c645a8373d2d0dd5278db68982808777c5
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-08-03 12:19:35 +0300

    creates the apps URLconfigs

--- booking/urls.py
+++ booking/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('bus_booking.urls'))
+    path('',include('adminapp.urls')),
+    path('',include('driverapp.urls')),
+    path('',include('userapp.urls')),
 ]
