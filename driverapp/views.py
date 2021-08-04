71205ba998d28c564d9fc254d03a47419e0168b9
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-08-04 23:35:40 +0300

    create a dashboard for the driver app

--- driverapp/views.py
+++ driverapp/views.py
@@ -3,9 +3,9 @@ from .models import Driver, Bus, Book
 
 
 # Create your views here.
+def dashboard(request):
+    return render(request,'driver/dashboard.html')
+
 def index(request):
     drivers = Driver.objects.all()
     return render(request, 'driver/index.html', {'drivers': drivers})
-
-def dashboard(request):
-    return render(request,'driver/dashboard.html')
