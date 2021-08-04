a120e60ca5750aa1bc466b1ac5f7f35479247815
Santa claus <santa@nortpole.com>
2021-08-04 18:38:18 +0300

    add login url

--- driverapp/urls.py
+++ driverapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns=[
-    path('login',views.login, name='login'),
-    path('',views.index, name='index'),    
+    path('',views.index, name='index'),
+    path('customer/',views.customer, name='customer'),
+    path('registeruser/',views.registeruser, name='register'),
+    path('loginpage/',views.loginpage, name='loginpage'),     
 ]
