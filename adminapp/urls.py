e33b5f7e58fe5097f9eb8291c28160a448942cc0
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-08-05 08:57:03 +0300

    adds update to setting

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,9 +4,8 @@ from django.conf.urls import url
 
 urlpatterns=[
 ]
-    path('',views.index, name='passengers-dash'),
-    path('busses/',views.busses, name='busses-dash'),
-]
+   path('',views.index, name='passengers-dash'),
+   path('busses/',views.busses, name='busses-dash'),
    url(r'^$',views.admin, name='admin'),
    url(r'^register/$', views.registerPage, name="register"),
    url(r'^login/$', views.loginPage, name="adminlogin"), 
