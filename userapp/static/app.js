6be93eafab15e613268a313271cb8a628c08b069
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-08-07 12:16:13 +0300

    create the ajax requests

--- userapp/static/app.js
+++ userapp/static/app.js
@@ -1,39 +1,44 @@
+
+$(document).ready(function() {
+
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const busSelect = document.getElementById('bus');
+const movieSelect = document.getElementById('movie');
+const book = document.querySelector('.book');
 
+var myarray =[]
 populateUI();
+let ticketPrice = +movieSelect.value;
 
-let ticketPrice = +busSelect.value;
-
-// Save selected bus index and price
-function setBusData(busIndex, busPrice) {
-  localStorage.setItem('selectedBusIndex', busIndex);
-  localStorage.setItem('selectedBusPrice', busPrice);
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
-// Update total and count
+// update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  //copy selected seats into arr
+  // map through array
+  //return new array of indexes
 
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
-  
-  setBusData(busSelect.selectedIndex, busSelect.value);
 }
 
-// Get data from localstorage and populate UI
+// get data from localstorage and populate ui
 function populateUI() {
   const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
@@ -42,31 +47,52 @@ function populateUI() {
     });
   }
 
-  const selectedBusIndex = localStorage.getItem('selectedBusIndex');
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
 
-  if (selectedBusIndex !== null) {
-    busSelect.selectedIndex = selectedBusIndex;
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
   }
 }
 
-// Bus select event
-busSelect.addEventListener('change', e => {
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
-  setBusData(e.target.selectedIndex, e.target.value);
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
 // Seat click event
-container.addEventListener('click', e => {
-  if (
-    e.target.classList.contains('seat') &&
-    !e.target.classList.contains('occupied')
-  ) {
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
 
     updateSelectedCount();
   }
 });
 
-// Initial count and total set
+// intial count and total
 updateSelectedCount();
+
+/* Ajax requests*/
+
+  $('.book').click(function(){
+      $.ajax({
+          url:'seats/',
+          
+          type:'post',
+          data:{
+            count:'count',
+            total:'total',
+    
+          
+          },
+    
+
+  })
+ 
+
+  })
+  })
+  
+
+
--- userapp/views.py
+++ userapp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from .models import Bus
 # Create your views here.
+
 def index(request):
+
     return render(request, 'index.html')
 
+@csrf_exempt
 def seats(request):
-    return render(request, 'seats.html')
+    seat=request.GET.get('count')
+    total=request.GET.get('total')
+    # no_of_seat=Bus.objects.filter()
+    print(seat, total)
 
+
+    return render(request, 'seats.html')
+    
 def login(request):
     return render(request, 'auth/login.html')
