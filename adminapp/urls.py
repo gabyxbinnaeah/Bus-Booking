40748335a0b28cf2a18499d7ac1f04241d585d5c
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-08-10 16:44:40 +0300

    merge

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import url
 urlpatterns=[
     path('',views.index, name='passengers-dash'),
     path('busses/',views.busses, name='busses-dash'),
-    url(r'^$',views.admin, name='admin'),
     url(r'^login/$', views.loginPage, name="adminlogin"), 
     url(r'^logout/$',views.logoutUser,name="logout"),
     url(r'^contact/$',views.contact,name="contact"),
@@ -23,7 +22,7 @@ urlpatterns=[
     path('create_order_bus/', views.create_bus, name="create_order_bus"),
     path('update_order_bus/<str:pk>/', views.update_bus, name="update_order_bus"),
     path('delete_order_bus/<str:pk>/', views.delete_bus, name="delete_order_bus"),
-    url(r'admin/',views.admin, name='admin'),
+   
     url(r'^register/$', views.registerPage, name="adminregister"),
    
 
